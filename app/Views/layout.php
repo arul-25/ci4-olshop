2dbbce32c114150a5df166727d22e08854216c7b
arul-25 <arulprojectdualima@gmail.com>
2021-03-22 17:25:46 +0900

    First Commit

---
+++ app/Views/layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('public/assets/bootstrap'); ?>/css/bootstrap.min.css">
+    <title><?= $title; ?></title>
+</head>
+
+<body>
+    <?= $this->include('navbar'); ?>
+
+    <?= $this->renderSection('content'); ?>
+
+    <script src="<?= base_url('public/assets/bootstrap/'); ?>/js/jquery-3.5.1.min.js"></script>
+    <script src="<?= base_url('public/assets/bootstrap/'); ?>/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
